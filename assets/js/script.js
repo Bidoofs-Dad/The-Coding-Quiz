42c3532e20253639e86fb1bea91155f5b1f9f25e
Zane Werle <zanewerle@gmail.com>
2023-03-24 17:53:38 -0500

    added in some code for a form that will most likely be the basis for the different questions, also added in some JavaScript that when clicking the 'Start Quiz' button, the h1 changes to different text and the paragraph text is removed.

---
+++ assets/js/script.js
@@ -0,0 +1,8 @@
+var h1El = document.querySelector("#butts")
+var startQuizEl = document.querySelector("#startQuiz")
+var paragraphEl = document.querySelector("#paragraph")
+
+startQuizEl.addEventListener("click", function(){
+    h1El.textContent = "TESTTTTTTT";
+    paragraphEl.textContent = null;
+})
