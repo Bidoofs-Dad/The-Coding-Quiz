7224c755bd3bd11a39e3c8f7a6cbe3df42314363
Zane Werle <zanewerle@gmail.com>
2023-03-24 18:32:47 -0500

    messed around with some more code, so the 'Start Quiz' button also disappears, currently trying to find a way to incorporate the form into it better.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,19 @@
 var h1El = document.querySelector("#butts")
 var startQuizEl = document.querySelector("#startQuiz")
 var paragraphEl = document.querySelector("#paragraph")
+var testEl = document.querySelector("#testForm")
 
+function iDunno(){
+    document.getElementById("testForm").style.visibility = "hidden";
+}
 startQuizEl.addEventListener("click", function(){
     h1El.textContent = "TESTTTTTTT";
     paragraphEl.textContent = null;
+    startQuizEl.setAttribute("style", "display:none");
+    
+    
 })
+    // test.setAttribute("style", "display:flex");
+    
+
+
