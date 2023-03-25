28f4f121e6778a1838f84e2065cd2814e58a0b5c
Zane Werle <zanewerle@gmail.com>
2023-03-25 15:16:09 -0500

    added code that makes it so the form is hidden, and once you click 'start quiz' the form appears with it's questions and own h1

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,17 +2,30 @@ var h1El = document.querySelector("#butts")
 var startQuizEl = document.querySelector("#startQuiz")
 var paragraphEl = document.querySelector("#paragraph")
 var testEl = document.querySelector("#testForm")
+var formEl = document.querySelector(".form")
 
 function iDunno(){
-    document.getElementById("testForm").style.visibility = "hidden";
+    formEl.setAttribute("style", "display:none");
 }
 startQuizEl.addEventListener("click", function(){
-    h1El.textContent = "TESTTTTTTT";
+    h1El.style.display = 'none';
+    // h1El.textContent = "TESTTTTTTT";
     paragraphEl.textContent = null;
     startQuizEl.setAttribute("style", "display:none");
-    
-    
+    testEl.setAttribute("style", "display:flex")
 })
+
+// function formHider(testEl){
+//     if (testEl.style.display === 'none') {
+//         // 👇️ this SHOWS the form
+//         testEl.style.display = 'flex';
+//       } else {
+//         // 👇️ this HIDES the form
+//         testEl.style.display = 'none';
+//       }  
+// }
+
+
     // test.setAttribute("style", "display:flex");
     
 
